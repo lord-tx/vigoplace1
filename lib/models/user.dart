bc52c368a4e9ff593f20fd2ad78e801ef3593bf7
mark jordan <thurggex@gmail.com>
2022-05-15 13:35:17 +0100

    feat (auth): create auth models.
    reviewed by: Mark Jordan (JP)

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,20 @@
+class LoginUser{
+  LoginUser({this.username, this.password});
+
+  String? username;
+  String? password;
+}
+
+class SignUpUser{
+  SignUpUser({this.fullName, this.username, this.email, this.dateOfBirth, this.password});
+
+  String? fullName;
+  String? username;
+  String? email;
+  String? dateOfBirth;
+  String? password;
+}
+
 class User{
   User({this.username, this.imageUrl});
 
