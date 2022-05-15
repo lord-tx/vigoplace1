03a97d52a7a9cd0168dda51f563f2d1eb31d40ab
mark jordan <thurggex@gmail.com>
2022-05-15 13:36:12 +0100

    chore (entry): modify entry to support validation and text obscuring.
    reviewed by: Mark Jordan (JP)

--- lib/widgets/vigo_entry.dart
+++ lib/widgets/vigo_entry.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class VigoEntry extends StatefulWidget {
-  const VigoEntry({Key? key, this.label, required this.icon, this.passwordInput = false, this.hintText}) : super(key: key);
+  const VigoEntry({Key? key, this.label, required this.icon, this.passwordInput = false, this.hintText, this.eController}) : super(key: key);
 
   final String? label;
   final String? hintText;
   final IconData icon;
+  final TextEditingController? eController;
   // final IconData suffixIcon;
   final bool passwordInput;
   @override
@@ -13,6 +14,9 @@ class VigoEntry extends StatefulWidget {
 }
 
 class _VigoEntryState extends State<VigoEntry> {
+
+  bool hidePassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,17 +28,35 @@ class _VigoEntryState extends State<VigoEntry> {
         borderRadius: BorderRadius.circular(4)
       ),
       child: TextFormField(
+        controller: widget.eController,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            print('Input cannot be empty');
+            return 'Input cannot be empty';
+          }
+          return null;
+        },
+        obscureText: widget.passwordInput && hidePassword,
         decoration: InputDecoration(
           prefixIcon: Icon(widget.icon),
           hintText: widget.hintText,
           labelText: widget.label,
-          border: OutlineInputBorder(
+          border: const OutlineInputBorder(
             borderSide: BorderSide(
               color: Color.fromRGBO(209, 210, 216, 1)
             )
           ),
           fillColor: Colors.black38,
-          suffixIcon: widget.passwordInput ? const Icon(Icons.remove_red_eye_outlined) : Icon(Icons.remove_red_eye_outlined, color: Colors.transparent,)
+          suffixIcon: widget.passwordInput ? GestureDetector(
+              onTap: (){
+                setState(() {
+                  hidePassword = hidePassword ? false : true;
+                });
+              },
+              child: hidePassword ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off)
+          ) : GestureDetector(
+            child: const Text(""),
+          )
         ),
       ),
     );
